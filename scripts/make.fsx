ef13c1532e5d4464033bb988868a29c1e339b040
Mehrshad <code.rezaei@gmail.com>
2023-11-13 16:19:39 +0330

    refactor(make.fsx): better return of prjFile
    Making prjFile path can be done using string formatting.

--- scripts/make.fsx
+++ scripts/make.fsx
@@ -34,12 +34,12 @@ type ProjectFile =
 let GetProject (projFile: ProjectFile) =
     let projFileName =
         match projFile with
-        | ConsoleFrontend -> Path.Combine("GWallet.Frontend.Console", "GWallet.Frontend.Console.fsproj")
-        | GtkFrontend -> Path.Combine("GWallet.Frontend.XF.Gtk", "GWallet.Frontend.XF.Gtk.fsproj")
-        | XFFrontend -> Path.Combine("GWallet.Frontend.XF", "GWallet.Frontend.XF.fsproj")
+        | ConsoleFrontend -> "GWallet.Frontend.Console"
+        | GtkFrontend -> "GWallet.Frontend.XF.Gtk"
+        | XFFrontend -> "GWallet.Frontend.XF"
 
     let prjFile =
-        Path.Combine("src", projFileName)
+        Path.Combine("src", projFileName, sprintf "%s.fsproj" projFileName)
         |> FileInfo
     if not prjFile.Exists then
         raise <| FileNotFoundException("Project file not found", prjFile.FullName)
